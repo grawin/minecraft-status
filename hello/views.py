0b2697851c5a1f8dea4079fd01463affe4f42720
metapod <ryangrawin@gmail.com>
2015-07-15 22:22:23 -0400

    Add MC Status code
    Add code to poll minecraft server status using Dinnerbone's
    [mcstatus](https://github.com/Dinnerbone/mcstatus.git)

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from mcstatus import MinecraftServer
+
 from .models import Greeting
 
+import json
+
 # Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+def index(request):		
+	# Get the server
+	server = MinecraftServer.lookup("www.yourServerHere.com:25565")
 
+	# Get the status and query data, requires two calls to get everything.
+	status = server.status()
+	query = server.query()
 
+	# Create the object to store all the server data.
+	req = {}
+		
+	req['playerCount'] = status.players.online
+	req['maxPlayers'] = status.players.max
+
+	req['playerNames'] = query.players.names
+	
+	req['version'] = query.software.version;
+	req['motd'] = query.motd;
+	req['map'] = query.map;
+
+	# Package the requested data into the callback for JSONP
+	callback = request.GET.get('callback', '')
+	response = json.dumps(req)
+	response = callback + '(' + response + ');'
+
+	return HttpResponse(response, content_type="application/json")
+		
 def db(request):
 
     greeting = Greeting()
